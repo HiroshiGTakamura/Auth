504fb010bcd6d51727e6e5d291d9bc615c156da6
HiroshiGTakamura <pedrohiroshigodenytakamura@gmail.com>
2022-08-22 08:48:10 -0300

    :)

--- app/src/main/java/com/example/autenticador/MainActivity.kt
+++ app/src/main/java/com/example/autenticador/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.autenticador
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -17,6 +22,43 @@ class MainActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         auth = Firebase.auth
 
+        val txtCadastroUsuas: TextView = findViewById(R.id.cadastroUsuas)
+
+        txtCadastroUsuas.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btLogar: Button = findViewById(R.id.butao1)
+        btLogar.setOnClickListener{
+            performLogin()
+        }
+
+    }
+
+    private fun performLogin() {
+        val email: EditText = findViewById(R.id.login)
+        val senha: EditText = findViewById(R.id.senha)
+
+        if(email.text.isEmpty() || senha.text.isEmpty()){
+            Toast.makeText( this,"Erro no prenchimento", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val inputemail = email.text.toString()
+        val inputsenha = senha.text.toString()
+
+        auth.signInWithEmailAndPassword(inputemail, inputsenha)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, PrincipalActivity2::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText( this,"Sucesso", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText( this,"Erro", Toast.LENGTH_SHORT).show()
+                }
+
+            }
 
     }
 }
--- app/src/main/java/com/example/autenticador/RegisterActivity.kt
+++ app/src/main/java/com/example/autenticador/RegisterActivity.kt
@@ -1,11 +1,54 @@
 package com.example.autenticador
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        val btcadar: Button = findViewById(R.id.butao2)
+        btcadar.setOnClickListener{
+            performLogin()
+        }
+    }
+
+    private fun performLogin() {
+        val email: EditText = findViewById(R.id.login)
+        val senha: EditText = findViewById(R.id.senha)
+
+        if(email.text.isEmpty() || senha.text.isEmpty()){
+            Toast.makeText( this,"Erro no prenchimento", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val inputemail = email.text.toString()
+        val inputsenha = senha.text.toString()
+
+        auth.createUserWithEmailAndPassword(inputemail, inputsenha)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText( this,"Sucesso", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText( this,"Erro", Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
